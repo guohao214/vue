b85f3211d3c728da6da6af2f84552bd5eee06b7b
郭浩 <guohao214@gmail.com>
2016-08-29 21:53:23 +0800

    第一次提交

---
+++ login/src/components/InputComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <input type={{ this.props.type }} class="input"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input {
+  border:1px solid #CCC;
+  color:red;
+  padding:8px 10px;
+  margin:10px;
+}
+</style>
